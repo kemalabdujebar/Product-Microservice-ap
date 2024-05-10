54a4827258d2d5ba21b7c6525f367422535f7169
kemalabdujebar <85251727+kemalabdujebar@users.noreply.github.com>
2024-05-10 21:27:32 +0300

    frist class

---
+++ src/main/java/com/kemal2/ProductMicroserviceap/ProductMicroserviceApApplication.java
@@ -0,0 +1,13 @@
+package com.kemal2.ProductMicroserviceap;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProductMicroserviceApApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProductMicroserviceApApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/kemal2/ProductMicroserviceap/ProductMicroserviceApApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kemal2.ProductMicroserviceap;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProductMicroserviceApApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
